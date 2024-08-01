8e3f76930404b16b96db9bf8e41806957112966a
Stanislav N. <stanislav_developer@mail.ru>
2024-08-02 00:02:04 +0500

    +

---
+++ Assets/Source/CodeBase/Environment/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Source.CodeBase.Environment
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private Transform _transform;
+        [SerializeField] private float _speed;
+        [SerializeField][Range(-1, 1)] private int _sideRotation;
+
+        private void Update()
+        {
+            Rotate();
+        }
+
+        private void Rotate()
+        {
+            float rotation = _sideRotation * _speed * Time.deltaTime;
+            _transform.Rotate(Vector3.up, rotation);
+        }
+    }
+}
